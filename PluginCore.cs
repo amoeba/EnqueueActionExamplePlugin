2f8125cd99631ecc15455563a243fba806c226ea
Bryce Mecum <petridish@gmail.com>
2019-12-09 21:35:30 -0900

    Initial commit (base empty plugin)

---
+++ PluginCore.cs
@@ -0,0 +1,42 @@
+﻿namespace EnqueueActionExamplePlugin
+{
+    [Decal.Adapter.FriendlyName("EnqeueActionExamplePlugin")]
+    public partial class PluginCore : Decal.Adapter.PluginBase
+    {
+        protected override void Startup()
+        {
+            try
+            {
+                Core.CharacterFilter.LoginComplete += CharacterFilter_LoginComplete;
+            }
+            catch (System.Exception ex)
+            {
+                // Nothing
+            }
+        }
+
+        protected override void Shutdown()
+        {
+            try
+            {
+                Core.CharacterFilter.LoginComplete -= CharacterFilter_LoginComplete;
+            }
+            catch (System.Exception ex)
+            {
+                // Nothing
+            }
+        }
+
+        private void CharacterFilter_LoginComplete(object sender, System.EventArgs e)
+        {
+            try
+            {
+                Core.Actions.AddChatText("EnqueueActionExamplePlugin.LoginComplete()", 1);
+            }
+            catch (System.Exception ex)
+            {
+                // Nothing
+            }
+        }
+    }
+}
